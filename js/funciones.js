2b1f6144b9a56fb7460040b25f573f6882339f7a
gboned <gboned@cifpfbmoll.eu>
2020-05-25 15:03:58 +0200

    Añadir array con la lista de posibles letras

--- js/funciones.js
+++ js/funciones.js
@@ -3,3 +3,7 @@
 /*          AYUDA A LA POLICÍA           */
 /*                                       */           
 /*****************************************/
+
+// Creo un array con la lista de posibles letras que puede contener un DNI.
+listaLetras = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", 
+                "V", "H", "L", "C", "K", "E", "T"];
